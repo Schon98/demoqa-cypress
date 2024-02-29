0d200a3ee3d1dc63ef7053d8c4dff2571e67eae9
Schon98 <84647547+Schon98@users.noreply.github.com>
2024-02-29 19:35:37 -0300

    teste

--- cypress/e2e/textbox.cy.js
+++ cypress/e2e/textbox.cy.js
@@ -8,7 +8,7 @@ describe("Testando elementos na demoqa", () => {
     cy.get(':nth-child(1) > .element-list > .menu-list > #item-0').click();
 
     /* ==== Generated with Cypress Studio ==== */
-    cy.get('#userName').type('Matheus Schon');
+    cy.get('#userName').type('Matheus Santana Schon');
     cy.get('#userEmail').type('matheusr@esparta.com');
     cy.get('#submit').click();
     /* ==== End Cypress Studio ==== */
